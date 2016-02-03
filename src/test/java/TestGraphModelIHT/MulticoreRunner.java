b2b570a74539a976c74029766b815fde14027f91
runninghack <zl.ramiel@gmail.com>
2016-02-02 20:27:46 -0500

    debug multicoreRunner.java

--- src/test/java/TestGraphModelIHT/MulticoreRunner.java
+++ src/test/java/TestGraphModelIHT/MulticoreRunner.java
@@ -25,79 +25,39 @@ import graphModelIHT.GraphModelIHT;
 public class MulticoreRunner {
 	//================== Setters and getters start =====================
 	private double max_score = 0;
-	public double getMax_score() {
-		return max_score;
-	}
-	public void setMax_score(double max_score) {
-		this.max_score = max_score;
-	}
+
 
 	private int[] best_subgraph;
-	public int[] getBest_subgraph() {
-		return best_subgraph;
-	}
-	public void setBest_subgraph(int[] best_subgraph) {
-		this.best_subgraph = best_subgraph;
-	}
+
 	
 	private List<String> ignore_words;
-	public List<String> getIgnore_words() {
-		return ignore_words;
-	}
-	public void setIgnore_words(List<String> ignore_words) {
-		this.ignore_words = ignore_words;
-	}
-	
+
+	private List<String> temp_words;
+
 	private boolean is_first_time;
-	public void setIs_first_time(boolean is_first_time) {
-		this.is_first_time = is_first_time;
-	}
-	public boolean getIs_first_time() {
-		return this.is_first_time;
-	}
+
 	
 	private String bestWindow;
-	public String getBestWindow() {
-		return bestWindow;
-	}
-	public void setBestWindow(String bestWindow) {
-		this.bestWindow = bestWindow;
-	}
+
 	
 	private List<String[]> subgraph;
-	public List<String[]> getSubgraph() {
-		return subgraph;
-	}
-	public void setSubgraph(List<String[]> subgraph) {
-		this.subgraph = subgraph;
-	}
-	
+
 	private List<Double> weights;
-	public List<Double> getWeights() {
-		return weights;
-	}
-	public void setWeights(List<Double> weights) {
-		this.weights = weights;
-	}
+
 	private String up_down;
-	public String getUp_down() {
-		return up_down;
-	}
-	public void setUp_down(String up_down) {
-		this.up_down = up_down;
-	}
+
 	//================== Setters and getters end =====================
 	public MulticoreRunner(){
-		this.setIs_first_time(true);
+		this.is_first_time = true;
 	}
 	
 	public MulticoreRunner(String month_folder, String result_folder, String up_down) throws IOException{
 		String month_path = month_folder;
 		String date = month_path.substring(month_path.lastIndexOf("/") - 8,month_path.lastIndexOf("/") );
-		this.setIs_first_time(true);
-		this.setUp_down(up_down);
+		this.is_first_time = true;
+		this.up_down = up_down;
 		ArrayList<String> templist = new ArrayList<String>() {{add("No words");}};
-		this.setIgnore_words(templist);
+		this.ignore_words.addAll(templist);
 
 		Writer fileWriter = new BufferedWriter(new OutputStreamWriter(
 			    new FileOutputStream(result_folder + date + "_results.txt"), "UTF-8"));
@@ -105,12 +65,12 @@ public class MulticoreRunner {
 		for(int i=0;i<5;i++){
 			System.out.println(i);
 			run_month(month_path);
-			String words_String= this.getIgnore_words().stream().collect(Collectors.joining("\t"));
-			fileWriter.write(String.valueOf(i) + "," + this.getBestWindow() + "," + words_String + "\n");
+			String words_String= this.temp_words.stream().collect(Collectors.joining("\t"));
+			fileWriter.write(String.valueOf(i) + "," + this.bestWindow + "," + words_String + "\n");
 			
-			writeGraph(result_folder + date + "_" + String.valueOf(i) + "_graph.txt", this.getIgnore_words(),this.getWeights(),this.getSubgraph());
+			writeGraph(result_folder + date + "_" + String.valueOf(i) + "_graph.txt", this.temp_words,this.weights,this.subgraph);
 			
-			this.setIs_first_time(false);
+			this.is_first_time=false;
 		}
 		fileWriter.close();
 	}
@@ -123,11 +83,13 @@ public class MulticoreRunner {
 		List<File> listOfFiles=Arrays.asList(files);
 		List<File> graphFiles = listOfFiles.stream()
 			    .filter(f -> f.getName().contains("graph")).collect(Collectors.toList());
-		this.setMax_score(0);
+		this.max_score = 0;
 		for(int i=0;i<graphFiles.size();i++){
 			String fname = graphFiles.get(i).getName();
 			this.run_timewindow(folder_path,fname);
 		}
+		this.ignore_words.addAll(this.temp_words);
+		this.temp_words = new ArrayList<String>();
 		long endTime=System.currentTimeMillis();
 		System.out.println("***" + folder_path + " done in:" + String.valueOf(endTime-startTime) + "***");
 	}
@@ -149,7 +111,7 @@ public class MulticoreRunner {
 			fw.close();
 		}
 		catch(NullPointerException ex){
-			fw.write("NO data");
+			fw.write("No data");
 			fw.close();
 		}
 	}
@@ -194,13 +156,13 @@ public class MulticoreRunner {
 		double[] c = apdm.getPValue();
 		
 		//detect only up or down
-		if(this.getUp_down().equals("down")){
+		if(this.up_down.equals("down")){
 			for(int i=0;i<c.length;i++){
 				if(c[i] > lambdas[i]){
 					c[i] = lambdas[i];
 				}
 			}
-		}else if(this.getUp_down().equals("up")){
+		}else if(this.up_down.equals("up")){
 			for(int i=0;i<c.length;i++){
 				if(c[i] < lambdas[i]){
 					c[i] = lambdas[i];
@@ -209,11 +171,12 @@ public class MulticoreRunner {
 		}
 		
 		//adjust most anomalous nodes in order to detect other anomalous nodes
-		if(!this.getIs_first_time()){
-			for(String iw: this.getIgnore_words()){
+		double[] c2 = new double[c.length];
+		if(!this.is_first_time){
+			for(String iw: this.ignore_words){
 				for(int i=0;i<apdm.numNodes;i++){
 					if(word_dict.getString(String.valueOf(i)).equals(iw)){
-						c[i] = lambdas[i];
+						c2[i] = lambdas[i];
 					}
 				}
 			}
@@ -221,10 +184,10 @@ public class MulticoreRunner {
 		
 		//sparsity s
 		//int s = apdm.trueSubGraphNodes.length ;
-		int s = 7;
+		int s = 8;
 				
 		GraphModelIHT graphModelIHT = 
-				new GraphModelIHT(edges, null, c, null, s/2, 1, s-1+0.0D, 10, false, null, null, null, R, lambdas) ;
+				new GraphModelIHT(edges, null, c2, null, s/2, 1, s-1+0.0D, 10, false, null, null, null, R, lambdas) ;
 		//post-process
 		List<Integer> index_list= IntStream.of(graphModelIHT.resultNodes_Tail).boxed().collect(Collectors.toList());
 		List<String> words = new ArrayList<String>();
@@ -241,13 +204,13 @@ public class MulticoreRunner {
 				sub_edges.add(new String[]{word_dict.getString(String.valueOf(e[0])),word_dict.getString(String.valueOf(e[1]))});
 			}
 		}
-		if(graphModelIHT.funcValue > this.getMax_score()){
-			this.setMax_score(graphModelIHT.funcValue);
-			this.setBest_subgraph(graphModelIHT.resultNodes_Tail);
-			this.setIgnore_words(words);
-			this.setBestWindow(filename);
-			this.setSubgraph(sub_edges);
-			this.setWeights(weights);
+		if(graphModelIHT.funcValue > this.max_score){
+			this.max_score = graphModelIHT.funcValue;
+			this.best_subgraph = graphModelIHT.resultNodes_Tail;
+			this.temp_words = words;
+			this.bestWindow = filename;
+			this.subgraph = sub_edges;
+			this.weights = weights;
 		}
 		long endTime2=System.currentTimeMillis();
 		//System.out.println(String.valueOf(endTime2-startTime));
@@ -278,7 +241,7 @@ public class MulticoreRunner {
 		long startTime=System.currentTimeMillis();
 		MulticoreRunner runner = new MulticoreRunner();
 		runner.run_timewindow(root, filename);
-		System.out.println(runner.getIgnore_words().stream().collect(Collectors.joining(", ")));
+		System.out.println(runner.temp_words.stream().collect(Collectors.joining(", ")));
 		long endTime2=System.currentTimeMillis();
 		//System.out.println(String.valueOf(endTime2-startTime));
 	}
